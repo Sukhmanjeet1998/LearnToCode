a8b7bc3ec32968b517d867cdc6bbba59306466da
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-04-09 13:35:10 +0000

    comparison

---
+++ ChaiAurCode/Javascript/Code/8_comparison.js
@@ -0,0 +1,23 @@
+// node ChaiAurCode/Javascript/Code/8_comparison.js
+
+// >, <, >=, <=, !=, ===, !===
+/*
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null != 0);
+*/
+/*
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);   
+*/
+/*
+console.log(2=="2");
+console.log(2==="2");
+*/
