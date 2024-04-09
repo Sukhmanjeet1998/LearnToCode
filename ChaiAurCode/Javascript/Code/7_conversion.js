e54e9ce31a9bd27728508fecf8a176f2e9eb0e21
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-04-09 13:24:27 +0000

    conversion

---
+++ ChaiAurCode/Javascript/Code/7_conversion.js
@@ -0,0 +1,31 @@
+// node ChaiAurCode/Javascript/Code/7_conversion.js
+
+console.log("Why string to number conversion is confusing!");
+/*
+let a = 5;
+let b = 2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(a%b);
+*/
+/*
+let str1 = "Sukhman";
+let str2 = "jeet";
+let str3 = " Singh";
+console.log(str1+str2+str3);
+*/
+/*
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log((3+4)*5%3);
+*/
+/*
+let num1, num2, num3;
+num1 = num2 = num3 = 5;
+console.log(num1, num2, num3);
+*/
