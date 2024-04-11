da1078689e2befd30368f6bb2cc88502a91b9fb1
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-04-11 13:47:52 +0000

    Destructure Object and API

--- ChaiAurCode/Javascript/Code/18_objects3.js
+++ ChaiAurCode/Javascript/Code/18_objects3.js
@@ -1,1 +1,23 @@
 // node ChaiAurCode/Javascript/Code/18_objects3.js
+
+// Object de-structure and JSON API intro
+
+const course = {
+    course : "JS in hindi",
+    price : 999,
+    courseInstructor : "John"
+};
+
+// New way to call object properties
+// {} : de-Structure
+const {courseInstructor: instructor} = course;
+console.log(instructor);
+
+// JOSN (keys are also string)
+/*
+{
+    "fname": "John",
+    "course": "JS in hindi",
+    "price": 999,
+}
+*/
