934312d24f0a0f5b5a3ab6d76b915de6cfb87217
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-04-16 17:07:05 +0000

    map, filter, reduce

--- ChaiAurCode/Javascript/Code/29_highorderarray.js
+++ ChaiAurCode/Javascript/Code/29_highorderarray.js
@@ -1,1 +1,160 @@
 // node ChaiAurCode/Javascript/Code/29_highorderarray.js
+
+/* Array specific loop */
+
+// ["","",""],   
+// [{},{},{},{}]
+
+// Iterator : objects, array, string
+
+// for of loop.............................
+// Array iterate through for of loop
+/*
+const myArr = [1,2,3,4,5];
+for (const value of myArr) {
+    console.log(value);
+}
+*/
+
+// String iterate through for of loop
+/*
+const greeting = "Hello Sir, Happy Birthday!";
+for (const valueG of greeting) {
+    console.log(valueG);
+}
+*/
+
+// Maps (Object)  iterate through for of loop
+/*
+const map = new Map();  // access object
+map.set("IN","INDIA");
+map.set("USA","United State of America");
+map.set("FR","FRANCE");
+map.set("IN","INDIA");
+
+console.log(map);
+
+for (const [keyM,valueM] of map) {
+   console.log(`${keyM} = ${valueM}`); 
+}
+*/
+
+// Objects are not iterate through :  for of loop
+/*
+const myInfo = {
+    fname : "John",
+    lname : "Doe",
+    age : 25
+};
+
+
+for (const [keyI,valueI] of myInfo) {
+    console.log(`${keyI},${valueI}`);
+}
+*/
+
+// FOR IN LOOP..............................
+// Objects are  iterate through : for in loop
+/*
+const myInfo = {
+    fname : "John",
+    lname : "Doe",
+    age : 25,
+    workProfile : "Developer",
+    salary : 25500.70
+};
+
+for (const key in myInfo) {
+    console.log(`${key} => ${myInfo[key]}`);
+}
+*/
+
+// Array also iterate through : for in loop
+/*
+const myArr = [1,2,3,4,5];
+for (const key in myArr) {
+    console.log(`${key} => ${myArr[key]}`);
+}
+*/
+
+// String also iterate through : for in loop
+/*
+const myName = "John Doe";
+for (const key in myName) {
+    console.log(`${key} => ${myName[key]}`);
+}
+*/
+
+// But Map are not iterate through :  for in loop  (Map are not iterable)
+/*
+const map = new Map();  // access object
+map.set("IN","INDIA");
+map.set("USA","United State of America");
+map.set("FR","FRANCE");
+map.set("IN","INDIA");
+
+for (const [key,value] in map) {
+    console.log(`${key} => ${value}`);
+}
+*/
+
+// Reason for Map 
+/*
+The for...in loop in JavaScript is primarily used to iterate over the enumerable properties of an object. It's not designed to iterate over the elements of an array.
+
+Array.prototype.map() is a higher-order function specifically designed for iterating over the elements of an array. It applies a callback function to each element of the array and returns a new array containing the results of calling the callback on each element.
+*/
+
+// For each loop...........................
+
+// Array
+/*
+const coding = ["Javascript","C++","Java","Python","Scala","Ruby","C"];
+
+coding.forEach((value, key)=>{
+    console.log(`${key}=>${value}`);
+});
+*/
+
+// Map
+/*
+const map = new Map();  // access object
+map.set("IN","INDIA");
+map.set("USA","United State of America");
+map.set("FR","FRANCE");
+map.set("IN","INDIA");
+
+// console.log(map);
+
+map.forEach((value, key)=>{
+    console.log(`${key}=>${value}`);
+});
+*/
+
+// Array with objects
+/*
+const myCoding = [
+    {
+        languageName : "Python",
+        languageFileName : "Python Snake",
+    },
+    {
+        languageName : "Jvascript",
+        languageFileName : "Web Development",
+    },
+    {
+        languageName : "C++",
+        languageFileName : "High Frequency Trading",
+    }
+];
+
+myCoding.forEach((values, keys)=>{
+    console.log(values.languageFileName);
+});
+*/
+
+// String (Not work)
+
+// Object (Not work)
+
+
--- ChaiAurCode/Javascript/Code/30_filtermap_reduce.js
+++ ChaiAurCode/Javascript/Code/30_filtermap_reduce.js
@@ -1,1 +1,149 @@
 // node ChaiAurCode/Javascript/Code/30_filtermap_reduce.js
+
+const coding = ["C++", "PHP", "Javascript"];
+
+// for each didn't return any value.
+/*
+coding.forEach( (value, key, myArr) => {
+    console.log(`${key} => ${value} , ${myArr}`);
+});
+*/
+
+// return filter array, filter()
+/*
+const myNum = [1,2,3,4,5,6,7,8,9,10];
+const evenNo = myNum.filter((value) => {
+            return (value%2==0)
+        });
+console.log(evenNo);
+*/
+
+/*
+const myCoding = [
+    {
+        languageName : "Python",
+        languageFileName : "Game Development",
+    },
+    {
+        languageName : "C#",
+        languageFileName : "Game Development",
+    },
+    {
+        languageName : "Javascript",
+        languageFileName : "Web Development",
+    },
+    {
+        languageName : "Typescript",
+        languageFileName : "Web Development",
+    },
+    {
+        languageName : "PHP",
+        languageFileName : "Web Development",
+    },
+    {
+        languageName : "Java",
+        languageFileName : "Web Development",
+    },
+    {
+        languageName : "C++",
+        languageFileName : "High Frequency Trading",
+    }
+];
+
+
+const language_ = myCoding.filter((value)=>{
+        return (value.languageFileName === "Web Development");
+});
+
+console.log(language_);
+*/
+
+
+const myNum_ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+/*
+const myNewArr = myNum_.map((value)=>{
+    return value/10;
+});
+
+console.log(myNewArr);
+*/
+
+/*
+myNum_.forEach((value)=>{
+     console.log( value+10);
+});
+*/
+
+/*
+const myNewArr = myNum_
+    .map((value) => value / 10)
+    .map((value) => value + 1)
+    .filter((value)=>value>1.5);
+
+console.log(myNewArr);
+*/
+
+// Reduce
+/*
+const myArr = [1,2,3,4,5];
+const initialise = 5;
+
+const sumOfArrayIs = myArr.reduce((accumulator, currentvalue)=>{
+    console.log(`acc: ${accumulator} , currV: ${currentvalue}`);
+    return (accumulator * currentvalue)
+},initialise);
+console.log(sumOfArrayIs);
+*/
+
+const myCodingPurchase = [
+    {
+        languageName : "Python",
+        languageFileName : "Game Development",
+        coursePrice : 1
+    },
+    {
+        languageName : "C#",
+        languageFileName : "Game Development",
+        coursePrice : 2
+    },
+    {
+        languageName : "Javascript",
+        languageFileName : "Web Development",
+        coursePrice : 3
+    },
+    {
+        languageName : "Typescript",
+        languageFileName : "Web Development",
+        coursePrice : 4
+    },
+    {
+        languageName : "PHP",
+        languageFileName : "Web Development",
+        coursePrice : 5
+    },
+    {
+        languageName : "Java",
+        languageFileName : "Web Development",
+        coursePrice : 6
+    },
+    {
+        languageName : "C++",
+        languageFileName : "High Frequency Trading",
+        coursePrice : 7
+    }
+];
+
+
+const totalCoursePrice = myCodingPurchase.reduce((accumulator,currentvalue)=>{
+        return accumulator + currentvalue.coursePrice;
+},0);
+
+console.log(totalCoursePrice);
+
+
+
+
+
+
+
+
