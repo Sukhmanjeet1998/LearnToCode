915b4b4aaab1cdfd6ada0c300cd213286101653f
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-04-16 11:39:36 +0000

    loops

--- ChaiAurCode/Javascript/Code/27_loop.js
+++ ChaiAurCode/Javascript/Code/27_loop.js
@@ -2,10 +2,54 @@
 
 // Iterations
 
+/*
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if (element === 5) {
+        console.log(`${element} is best number!`);
+    }
+    console.log(index);
+}
+*/
 
+/*
+In JavaScript, variables declared with const are block-scoped, meaning their scope is limited to the block in which they are defined. In your code snippet, the const element declaration is within the scope of the for loop block. Therefore, a new element variable is created and initialized with the value of index in each iteration of the loop.
 
+Even though the value of index changes with each iteration, the const declaration ensures that a new element variable is created and scoped to each iteration of the loop, and it retains its value for the duration of that iteration. This behavior is consistent with how block-scoped variables work in JavaScript.
+*/
 
+/*
+for (let index = 1; index <= 10; index++) {
+    // console.log(`........Table of ${index}!`);
+    for (let jndex = 1; jndex <= 10; jndex++) {
+        // console.log(`${index} X ${jndex} = ${index*jndex}`);
+    }
+}
+*/
 
+/*
+const myArr = ["flash", "batman", "superman"];
+for (let index = 0; index < myArr.length; index++) {
+    console.log(`${index} => ${myArr[index]}`);
+}
+*/
 
+// Break and Continue
 
+/*
+const myArr = [1,2,3,4,5,6,undefined,8,9,10,11,undefined,13,14,undefined,16];
+const newArr = [];
 
+for (let index = 0; index < myArr.length; index++) {
+    newArr[index] = myArr[index];
+    // console.log(`${index} = ${newArr[index]}`);
+    if (myArr[index] === undefined) {
+        newArr[index] = index+1;
+    }
+    else if(myArr[index] >= 10){
+        break;
+    }
+}
+
+console.log(newArr);
+*/
--- ChaiAurCode/Javascript/Code/28_while.js
+++ ChaiAurCode/Javascript/Code/28_while.js
@@ -1,1 +1,26 @@
 // node ChaiAurCode/Javascript/Code/28_while.js
+/*
+let i = 1;
+while (i<=10) {
+    console.log(`77 X ${i} = ${77*i}`);
+    i++;
+}
+*/
+/*
+const myArr = [123,143,165,777,333,564,876,367,34576,335,146,999];
+let j = 0;
+while (j<myArr.length) {
+    console.log(`${j} = ${myArr[j]}`);
+    j++;
+}
+*/
+
+// Do while
+/*
+let age = 15;
+do {
+    console.log(age);
+    age++;
+} while (age>=18);
+*/
+
